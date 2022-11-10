39e3070708d9f2910e0cc28bc1d668e8d681c12c
dap0vich <d4nil.pichugin@yandex.ru>
2022-11-10 17:47:19 +0700

    [refactoring]: fix elif conditions

--- 03-command-flow/fizz-buzz.py
+++ 03-command-flow/fizz-buzz.py
@@ -2,9 +2,9 @@
 def check(number):
     if (number % 3 == 0) and (number % 5 == 0):
         return "Fizz Buzz"
-    elif (number % 3 == 0) and (number % 5 != 0):
+    elif (number % 3 == 0):
         return "Fizz"
-    elif (number % 3 != 0) and (number % 5 == 0):
+    elif (number % 5 == 0):
         return "Buzz"
     else:
         return str(number)

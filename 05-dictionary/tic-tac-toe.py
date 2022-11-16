8b3839bae022db6f3ec29b6488827c0f7a04c86a
dap0vich <d4nil.pichugin@yandex.ru>
2022-11-16 16:48:06 +0700

    can't come up with a idea to solve the task

---
+++ 05-dictionary/tic-tac-toe.py
@@ -0,0 +1,19 @@
+# Consider the result of the tic-tac-toe game
+
+
+def to_determine_result(data: list[str]) -> str:
+    rows = []
+    columns = []
+
+
+def main():
+    data = [
+        "X.O",
+        "XX.",
+        "XOO"
+    ]
+    print(to_determine_result(data))
+
+
+if __name__ == '__main__':
+    main()

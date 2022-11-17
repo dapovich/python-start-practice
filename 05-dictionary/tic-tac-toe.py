5dc9dfa6d055db0dcea8d16981fddf1d34e64cf8
dap0vich <d4nil.pichugin@yandex.ru>
2022-11-17 19:53:50 +0700

    fix program: make it worked correctly, if there is an empty field

--- 05-dictionary/tic-tac-toe.py
+++ 05-dictionary/tic-tac-toe.py
@@ -4,15 +4,15 @@
 def to_determine_result(data: list[str]) -> str:
     for row in range(len(data)):
         # check for rows
-        if data[row][0] == data[row][1] == data[row][2]:
+        if data[row][0] == data[row][1] == data[row][2] != '.':
             return f"-> {data[row][0]}"
         # check for columns
-        if data[0][row] == data[1][row] == data[2][row]:
+        if data[0][row] == data[1][row] == data[2][row] != '.':
             return f"-> {data[0][row]}"
     # check diagonals
-    if data[0][0] == data[1][1] == data[2][2]:
+    if data[0][0] == data[1][1] == data[2][2] != '.':
         return f"-> {data[0][0]}"
-    if data[2][0] == data[1][1] == data[0][2]:
+    if data[2][0] == data[1][1] == data[0][2] != '.':
         return f"-> {data[2][0]}"
 
     # if none of the conditions passed
@@ -31,12 +31,18 @@ def main():
     ]
     data_test3 = [
         "OOX",
-        "XXO",
+        "XX.",
         "OXX"
     ]
-    print("Test 1:",to_determine_result(data_test1))
-    print("Test 2:",to_determine_result(data_test2))
-    print("Test 3:",to_determine_result(data_test3))
+    data_test4 = [
+        ".O.",
+        "..O",
+        ".XX"
+    ]
+    print("Test 1:", to_determine_result(data_test1))
+    print("Test 2:", to_determine_result(data_test2))
+    print("Test 3:", to_determine_result(data_test3))
+    print("Test 4:", to_determine_result(data_test4))
 
 
 if __name__ == '__main__':

cd7d614d8174d08b2591c69d0545c3a7415973c6
dap0vich <d4nil.pichugin@yandex.ru>
2022-11-17 15:46:52 +0700

    make tic-tac-toe task

--- 05-dictionary/tic-tac-toe.py
+++ 05-dictionary/tic-tac-toe.py
@@ -2,17 +2,41 @@
 
 
 def to_determine_result(data: list[str]) -> str:
-    rows = []
-    columns = []
+    for row in range(len(data)):
+        # check for rows
+        if data[row][0] == data[row][1] == data[row][2]:
+            return f"-> {data[row][0]}"
+        # check for columns
+        if data[0][row] == data[1][row] == data[2][row]:
+            return f"-> {data[0][row]}"
+    # check diagonals
+    if data[0][0] == data[1][1] == data[2][2]:
+        return f"-> {data[0][0]}"
+    if data[2][0] == data[1][1] == data[0][2]:
+        return f"-> {data[2][0]}"
 
+    # if none of the conditions passed
+    return "-> D"
 
 def main():
-    data = [
+    data_test1 = [
         "X.O",
         "XX.",
         "XOO"
     ]
-    print(to_determine_result(data))
+    data_test2 = [
+        "OO.",
+        "XOX",
+        "XOX"
+    ]
+    data_test3 = [
+        "OOX",
+        "XXO",
+        "OXX"
+    ]
+    print("Test 1:",to_determine_result(data_test1))
+    print("Test 2:",to_determine_result(data_test2))
+    print("Test 3:",to_determine_result(data_test3))
 
 
 if __name__ == '__main__':

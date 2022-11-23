422e914fd94dbaa3c633c057a984d4aa69cb7695
dap0vich <d4nil.pichugin@yandex.ru>
2022-11-23 13:56:47 +0700

    fix problem with roman_numerals dictionary

--- 05-dictionary/roman-numerals.py
+++ 05-dictionary/roman-numerals.py
@@ -4,21 +4,19 @@ from collections import OrderedDict
 
 # Global dictionary of roman_numerals
 roman_numerals = OrderedDict()
-roman_numerals = {
-    1000 : 'M',
-    900 : 'CM',
-    500 : 'D',
-    400 : 'CD',
-    100 : 'C',
-    90 : 'XC',
-    50 : 'L',
-    40 : 'XL',
-    10 : 'X',
-    9 : 'IX',
-    5 : 'V',
-    4 : 'IV',
-    1 : 'I',
-}
+roman_numerals[1000] = 'M'
+roman_numerals[900] = 'CM'
+roman_numerals[500] = 'D'
+roman_numerals[400] = 'CD'
+roman_numerals[100] = 'C'
+roman_numerals[90] = 'XC'
+roman_numerals[50] = 'L'
+roman_numerals[40] = 'XL'
+roman_numerals[10] = 'X'
+roman_numerals[9] = 'IX'
+roman_numerals[5] = 'V'
+roman_numerals[4] = 'IV'
+roman_numerals[1] = 'I'
 
 
 def to_roman(number: int) -> str:
